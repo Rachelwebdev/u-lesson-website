b9da2715c4830c2cb6eec04f1dada183a67c5a52
Rachel Isaac <rachelisaac995@gmail.com>
2023-06-08 20:40:10 +0400

    Add react app set up

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# u-lesson-website
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ULESSON</h1>
+    </div>
+  );
+}
+
+export default App;
