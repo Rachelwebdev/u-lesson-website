c312eec4928674d7808dcb9fb446b9991140e462
Rachel Isaac <rachelisaac995@gmail.com>
2023-06-09 10:47:23 +0400

    Add deducation component and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import PageNav from './components/Navbar';
+import EduTab from './components/EduTab';
 
 function App() {
   return (
     <>
       <PageNav />
+      <EduTab />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import '../Navbar.css';
+import '../styles/Navbar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
